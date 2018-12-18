2286ce4fbdeaefdd390e28b2fadf65eccd5ad97f
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-12-18 15:16:05 +0100

    Opgave 6 er bygget videre - Der initieres nu et object via Media() constructoren i Media klassen

--- src/MediaManager.java
+++ src/MediaManager.java
@@ -19,16 +19,16 @@ public class MediaManager {
 
     }
 
-    public static void readMediaFolder(String folderNavn)   {
+    public static File[] readMediaFolder(String folderNavn)   {
         File folder = new File(folderNavn);
         File[] listOfFiles = folder.listFiles();
+        for (int j = 0; j < listOfFiles.length; j++)   {
+             if (j < listOfFiles.length)
+                new Media();
+              }
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
+        return listOfFiles;
+
         }
 
     }
